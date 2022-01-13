bc0bcfee533672a587832cba8bdf2d8c91e47657
unknown <marcin.loch23@gmail.com>
2022-01-14 00:22:05 +0100

    fcm

--- models/fcm.js
+++ models/fcm.js
@@ -4,6 +4,12 @@ const db = require('../config/database')
 const Fcm = db.define('fcms', {
     token: {
         type: Sequelize.STRING
+    },
+    createdAt: {
+        type: Sequelize.DATE
+    },
+    updatedAt: {
+        type: Sequelize.DATE
     }
 })
 
