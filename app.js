89df9108b493a69cdf12d77ad332404512e059f2
Marcin Łoch <m.loch@sedziszow.pseap.local>
2022-05-19 13:56:02 +0200

    cors

--- app.js
+++ app.js
@@ -13,22 +13,35 @@ const port = 5000
 
 const app = express()
 
-app.use(bodyParser.json({limit:'100mb'}))
-app.use(bodyParser.urlencoded({extended: true, limit:'100mb'}))
+app.use(bodyParser.json({ limit: '100mb' }))
+app.use(bodyParser.urlencoded({ extended: true, limit: '100mb' }))
 app.use(cookieParser())
 
 app.use(
     cors({
+        origin: [
+            "http://51.83.134.120",
+            "http://tebuty.pl",
+            "http://motopres.tebuty.pl",
+            "http://localhost:3000",
+            "http://127.0.0.1:3000",
+            "http://192.168.1.143:8081"
+        ],
+        methods: ['GET', 'PUT', 'POST'],
+        allowedHeaders: ['Content-Type', 'Authorization', 'x-csrf-token'],
+        credentials: true
+
+
+
         // origin: '*',
-        credentials: true,
-        // credentials: 'include',
-        "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
-        // 'Access-Control-Allow-Origin': '*',
-        // 'Access-Control-Allow-Private-Network': true,
-        headers: "Origin, X-Requested-With, Content-Type, Accept",
-        "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept",
+        // credentials: true,
+        // // credentials: 'include',
+        // "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+        // // 'Access-Control-Allow-Origin': '*',
+        // // 'Access-Control-Allow-Private-Network': true,
+        // headers: "Origin, X-Requested-With, Content-Type, Accept",
+        // "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept",
         // 'Access-Control-Allow-Origin': ["http://51.83.134.120","http://tebuty.pl","http://motopres.tebuty.pl","http://localhost:3000","http://127.0.0.1:3000","http://192.168.1.143:8081"],
-        origin: ["http://51.83.134.120","http://tebuty.pl","http://motopres.tebuty.pl","http://localhost:3000","http://127.0.0.1:3000","http://192.168.1.143:8081"],
         // origin: true
     })
 )
@@ -37,11 +50,11 @@ app.use(
 express.static(path.join(__dirname, '/uploads'));
 
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin","http://192.168.1.143:8081", "http://127.0.0.1:3000"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
+// app.use(function (req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "http://192.168.1.143:8081", "http://127.0.0.1:3000"); // update to match the domain you will make the request from
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+// });
 
 // Database 
 const db = require('./config/database')
@@ -49,12 +62,12 @@ const db = require('./config/database')
 db.authenticate()
     .then(() => console.log('Connection has been established successfully.'))
     .catch(err => console.error('Unable to connect to the database:', err))
-    
+
 
 
 
 app.use('/', routes)
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log("serwer działa na porcie " + port)
 })
